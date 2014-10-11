0a2efa0770efe0a7234a3ee7d9317649d42734a7
Patrick Morgan <patrick@patrick-morgan.net>
2014-10-11 14:47:50 -0500

    zsh support

---
+++ arid.env.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export EDITOR=vim
+export ARID_HOME=~/.arid
+export PATH=$PATH:$ARID_HOME
+
+source $ARID_HOME/.shell_aliases
+source $ARID_HOME/bin/git-completion.bash
+source $ARID_HOME/bin/rake-completion.bash
+source $ARID_HOME/bin/tmuxinator.bash
+source $ARID_HOME/powerline/powerline/bindings/bash/powerline.sh
---
+++ arid.env.zsh
@@ -0,0 +1,7 @@
+export EDITOR=vim
+export ARID_HOME=$HOME/.arid
+export PATH=$PATH:$ARID_HOME
+
+source $ARID_HOME/.shell_aliases
+source $ARID_HOME/bin/tmuxinator.zsh
+source $ARID_HOME/powerline/powerline/bindings/zsh/powerline.zsh
