b2322a449b2fbf56c2f016d402d983bcc0376012
Patrick Morgan <patrick@patrick-morgan.net>
2015-04-20 15:59:50 -0500

    update path to include arid/bin. Add "since" time helper script

--- arid.env.zsh
+++ arid.env.zsh
@@ -1,6 +1,6 @@
 export EDITOR=vim
 export ARID_HOME=$HOME/.arid
-export PATH=$PATH:$ARID_HOME
+export PATH=$PATH:$ARID_HOME:$ARID_HOME/bin
 
 source $ARID_HOME/.shell_aliases
 source $ARID_HOME/bin/tmuxinator.zsh
