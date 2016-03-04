422c0a66e7fa41d8c021f56d5dc0d3fe10244ad8
Patrick Morgan <patrick@patrick-morgan.net>
2016-03-04 08:50:08 -0600

    refactored exports; updated .gitconfig

--- arid.env.bash
+++ arid.env.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export EDITOR=vim
 export ARID_HOME=~/.arid
 export PATH=$PATH:$ARID_HOME
 
+source $ARID_HOME/.exports
 source $ARID_HOME/.shell_aliases
 source $ARID_HOME/bin/git-completion.bash
 source $ARID_HOME/bin/rake-completion.bash
--- arid.env.zsh
+++ arid.env.zsh
@@ -1,7 +1,7 @@
-export EDITOR=vim
 export ARID_HOME=$HOME/.arid
 export PATH=$PATH:$ARID_HOME:$ARID_HOME/bin
 
+source $ARID_HOME/.exports
 source $ARID_HOME/.shell_aliases
 source $ARID_HOME/bin/tmuxinator.zsh
 source $ARID_HOME/powerline/powerline/bindings/zsh/powerline.zsh
