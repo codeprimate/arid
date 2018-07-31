d83c8a10fc50f5a9eef794202fd192ac84fadf2b
Patrick Morgan <patrick@patrick-morgan.net>
2018-07-31 09:58:38 -0500

    updates

--- arid.env.bash
+++ arid.env.bash
@@ -4,7 +4,6 @@ export ARID_HOME=~/.arid
 export PATH=$PATH:$ARID_HOME
 
 source $ARID_HOME/.paths
-source $ARID_HOME/.exports
 source $ARID_HOME/.shell_aliases
 source $ARID_HOME/bin/git-completion.bash
 source $ARID_HOME/bin/rake-completion.bash
--- arid.env.zsh
+++ arid.env.zsh
@@ -4,7 +4,6 @@ export ARID_HOME=$HOME/.arid
 export PATH=$PATH:$ARID_HOME:$ARID_HOME/bin
 
 source $ARID_HOME/.paths
-source $ARID_HOME/.exports
 source $ARID_HOME/.development
 source $ARID_HOME/.shell_aliases
 source $ARID_HOME/.tools
